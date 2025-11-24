ac8298db4f5d7c19699951eff72947df58674d90
Gabriel <gabrielocdev595@gmail.com>
2025-11-23 22:01:05 -0300

    Requisitos e informações do projeto

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+    Este projeto requer conhecimento em: Python, lógica de programação.
+
+    Instruções iniciais:
+        Desenvolver uma agenda para salvar, editar, deletar e marcar um contato como favorito. O resultado da aplicação deve ser apresentado no terminal.
+    
+    Requisitos: 
+        - Mostrar uma lista de opções do que é possível fazer com o app e permitir que o usuário digite uma escolha para iniciar a aplicação.
+        - Implementar funcionalidades para adicionar um contato (Nome, Telefone, Email, Favorito)
+        - Desenvolver visualização da lista de contatos cadastrados.
+        - Criar funcionalidades para editar um contato existente.
+        - Implementar opção para marcar/desmarcar um contato como favorito.
+        - Desenvolver visualização da lista de contatos.
+        - Criar funcionalidade para apagar um contato. 
+"""
+
